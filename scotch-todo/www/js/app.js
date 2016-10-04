35115b457047b865ed2f7cc8533be3eacddfbd14
Isabelle Silvestre <isaysilvestre@gmail.com>
2016-10-03 21:34:23 -0700

    Update app.js

--- scotch-todo/www/js/app.js
+++ scotch-todo/www/js/app.js
@@ -1,62 +1,79 @@
-var app = angular.module('scotch-todo', ['ionic', 'LocalStorageModule']);
+var app = angular.module('ionic-todo', ['ionic', 'LocalStorageModule']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
 
 app.config(function (localStorageServiceProvider) {
     localStorageServiceProvider
-      .setPrefix('scotch-todo');
-  });
-
-
-
-app.controller('main', function ($scope, $ionicModal, localStorageService) { //store the entities name in a variable var taskData = 'task';
-
-//initialize the tasks scope with empty array
-$scope.tasks = [];
-
-//initialize the task scope with empty object
-$scope.task = {};
-
-//configure the ionic modal before use
-$ionicModal.fromTemplateUrl('new-task-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-}).then(function (modal) {
-    $scope.newTaskModal = modal;
+        .setPrefix('ionic-todo')
 });
 
-$scope.getTasks = function () {
-          //fetches task from local storage
-          if (localStorageService.get(taskData)) {
-              $scope.tasks = localStorageService.get(taskData);
-          } else {
-              $scope.tasks = [];
-          }
-   }
+app.controller('main', function ($scope, $ionicModal, localStorageService) {
+    //store the entities name in a variable
+
+    var taskData = 'task';
+
+    //initialize the tasks scope with empty array
+    $scope.tasks = [];
+
+    //initialize the task scope with empty object
+    $scope.task = [];
+
+    //configure the ionic modal before use
+    $ionicModal.fromTemplateUrl('new-task-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.newTaskModal = modal;
+    });
+
+    $scope.getTasks = function () {
+        //fetches task from local storage
+        if (localStorageService.get(taskData)) {
+            $scope.tasks = localStorageService.get(taskData);
+        } else {
+            $scope.tasks = [];
+        }
+    };
+
+    $scope.createTask = function () {
+        //creates a new task
+        $scope.tasks.push($scope.task);
+        localStorageService.set(taskData, $scope.tasks);
+        $scope.task = {};
+
+        //close new task modal
+        $scope.newTaskModal.hide();
+    };
+
+    $scope.removeTask = function (index) {
+        //removes a task
+        $scope.tasks.splice(index, 1);
+        localStorageService.set(taskData, $scope.tasks);
+    };
 
 
-$scope.createTask = function () {
-          //creates a new task
-          $scope.tasks.push($scope.task);
-          localStorageService.set(taskData, $scope.tasks);
-          $scope.task = {};
-          //close new task modal
-          $scope.newTaskModal.hide();
-   }
+    $scope.completeTask = function (index) {
+        //updates a task as completed
+        localStorageService.set(taskData, $scope.tasks);
+    };
 
+    $scope.openTaskModal = function () {
+        $scope.newTaskModal.show();
+    };
 
-$scope.removeTask = function (index) {
-          //removes a task
-          $scope.tasks.splice(index, 1);
-          localStorageService.set(taskData, $scope.tasks);
-     }
-
-
-$scope.completeTask = function (index) { 
- //updates a task as completed 
- if (index !== -1) {
-  $scope.tasks[index].completed = true; 
- } 
-
-  localStorageService.set(taskData, $scope.tasks); 
-}
-
-})
+    $scope.closeTaskModal = function () {
+        $scope.newTaskModal.hide();
+    };
+});
